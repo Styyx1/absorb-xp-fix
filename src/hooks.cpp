6d31e77a33682d6eb1cbabe03344e2f02c385bda
Styyx <styyx228@gmail.com>
2025-03-05 08:43:49 +0100

    finished mod

--- src/hooks.cpp
+++ src/hooks.cpp
@@ -1,5 +1,7 @@
 #include "hooks.h"
 
+
+
 namespace Hooks 
 {
 	void Install() {
@@ -14,17 +16,18 @@ namespace Hooks
 	{		
 		if (a_this->spell->GetCastingType() == RE::MagicSystem::CastingType::kConcentration) {
 			if (a_this->spell->GetCostliestEffectItem()->baseEffect->HasArchetype(RE::EffectSetting::Archetype::kAbsorb)) {
+				// Credits: https://github.com/shad0wshayd3-TES5/BakaBloodMagic/blob/4fbfc17ed2c5309af3d22abd48d3c2f10056e9e4/src/BloodMagic/ActorMagicCaster.h#L507
 				RE::MagicItem::SkillUsageData SkillUsageData{};
-				a_this->spell->GetSkillUsageData(SkillUsageData);
-				if (!SkillUsageData.custom)
-				{
-					a_this->caster.get()->UseSkill(
-						SkillUsageData.skill,
-						std::clamp(std::clamp(std::pow(SkillUsageData.magnitude, 2), 0.0, 100.0) * a_delta, 0.0, 10.0),
-						nullptr);
-				}
+				if (a_this->spell->GetSkillUsageData(SkillUsageData)) {
+					if (!SkillUsageData.custom)
+					{
+						a_this->caster.get()->UseSkill(
+							SkillUsageData.skill,
+							std::clamp(std::clamp(std::pow(SkillUsageData.magnitude, 2), 0.0, 100.0) * a_delta, 0.0, 10.0),
+							nullptr);
+					}
+				}				
 			}
-
 		}
 		func(a_this, a_delta);
 	}
